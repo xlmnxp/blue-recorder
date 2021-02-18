595f831a4e90bd72af95900e249e2dd7e00a2ae8
Salem Yaslem <s@sy.sa>
2021-02-18 12:09:01 +0300

    initialization support for wayland

--- src/ffmpeg_interface.rs
+++ src/ffmpeg_interface.rs
@@ -1,14 +1,43 @@
 extern crate subprocess;
+// extern crate dbus;
 use chrono::prelude::*;
 use gtk::{
     CheckButton, ComboBoxExt, ComboBoxText, Entry, EntryExt, FileChooser, FileChooserExt,
     SpinButton, SpinButtonExt, ToggleButtonExt,
 };
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
 use subprocess::Exec;
+use zbus::dbus_proxy;
+use zvariant::Value;
+
+trait GnomeScreencastResult {}
+
+#[dbus_proxy(
+    interface = "org.gnome.Shell.Screencast",
+    default_path = "/org/gnome/Shell/Screencast"
+)]
+trait GnomeScreencast {
+    fn screencast(
+        &self,
+        file_template: &str,
+        options: HashMap<&str, Value>,
+    ) -> zbus::Result<(bool, String)>;
+
+    fn screencast_area(
+        &self,
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
+        file_template: &str,
+        options: HashMap<&str, Value>,
+    ) -> zbus::Result<(bool, String)>;
+    fn stop_screencast(&self) -> zbus::Result<bool>;
+}
 
 #[derive(Clone)]
 pub struct Ffmpeg {
@@ -28,11 +57,9 @@ pub struct Ffmpeg {
 impl Ffmpeg {
     pub fn start_record(&mut self, x: u16, y: u16, width: u16, height: u16) -> u32 {
         if self.process_id.is_some() {
-            Command::new("kill")
-                .arg(format!("{}", self.process_id.unwrap()))
-                .output()
-                .unwrap();
+            self.clone().stop_record();
         }
+        // self.clone().record_wayland();
 
         let mut ffmpeg_command: Command = Command::new("ffmpeg");
 
@@ -124,6 +151,30 @@ impl Ffmpeg {
         if !(self.command.get_text().trim() == "") {
             Exec::shell(self.command.get_text().trim()).popen().unwrap();
         }
+    }
+
+    // Gnome screencast for record wayland
+    pub fn record_wayland(self) {
+        // create new dbus session
+        let connection = zbus::Connection::new_session().unwrap();
+
+        // bind the connection to gnome screencast proxy
+        let gnome_screencast_proxy = GnomeScreencastProxy::new(&connection).unwrap();
+        
+        // options for gnome screencast
+        let mut screencast_options: HashMap<&str, Value> = HashMap::new();
+        screencast_options.insert("framerate", Value::new(self.record_frames.get_value()));
+        screencast_options.insert("draw-cursor", Value::new(self.record_mouse.get_active()));
+        gnome_screencast_proxy
+            .screencast(
+                "/home/xlmnxp/Projects/blue-recorder/test/test.webm",
+                screencast_options,
+            )
+            .unwrap();
+
+        // for testing it will record 20 seconds then stop the recording
+        sleep(Duration::from_secs(20u64));
+        gnome_screencast_proxy.stop_screencast().unwrap();
     }
 
     pub fn play_record(self) {
