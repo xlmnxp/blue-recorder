c6cf91e27ee2956df3cb42465a300b3d0c1c795c
ochibani <11yzyv86j@relay.firefox.com>
2025-01-01 04:53:22 +0200

    update

--- gui/src/ui.rs
+++ gui/src/ui.rs
@@ -1,7 +1,6 @@
-use adw::{Application, Window};
 use adw::gio::File;
-use adw::gtk::{AboutDialog, Builder, Button, CheckButton, ComboBoxText, CssProvider, Entry, FileChooserNative,
-                      FileChooserAction, Image, Label, MessageDialog, SpinButton, TextBuffer, TextView, ToggleButton};
+use adw::gtk::{AboutDialog, Application, Builder, Button, CheckButton, ComboBoxText, CssProvider, Entry, FileChooserNative,
+               FileChooserAction, Image, Label, MessageDialog, SpinButton, TextBuffer, TextView, ToggleButton, Window};
 use adw::prelude::*;
 use anyhow::Result;
 #[cfg(any(target_os = "freebsd", target_os = "linux"))]
@@ -496,6 +495,10 @@ fn build_ui(application: &Application, error_dialog: MessageDialog, error_messag
     );
     folder_chooser_native.set_transient_for(Some(&main_window));
     folder_chooser_native.set_modal(true);
+    let file_gio = &File::for_path(&config_management::get(
+            "default", "folder",
+        ));
+    println!("{file_gio}");
     folder_chooser_native
         .set_file(&File::for_path(&config_management::get(
             "default", "folder",
